67e821b75823b218d48d222849d5c2136cc30d7a
dgray11 <112587140+dgray11@users.noreply.github.com>
2022-09-06 22:07:04 +0000

    I did the Exercise: Working with Data

---
+++ ch01/exercises/simple.py
@@ -0,0 +1,19 @@
+print("this is some python code")
+print(10*5)
+print(10**2)
+print(15/10)
+print(15//10)
+print(-15//10)
+print(15%10)
+print(10%15)
+print(10%10)
+print(0%10)
+print(10/15)
+#the last answer keeps repeating
+rate = input("What is the current exchange rate for the Euro to Dollar?: ")
+rate = float(rate)
+amount = input("What is the amount of currency to exchange and save?: ")
+amount = int(amount)
+total = rate*amount
+result = total-3
+print(result)
