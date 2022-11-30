0134d338a0ca99d13ecc34f9f6dd109355562c3b
dgray11 <112587140+dgray11@users.noreply.github.com>
2022-11-30 23:58:17 +0000

    NA

--- ch09/exercises/main.py
+++ ch09/exercises/main.py
@@ -1,8 +1,1 @@
-import requests
-
-#facts = requests.get("https://dog-api.kinduff.com/api/facts").json()
-#print(facts.get("facts"))
-
-pic = requests.get("http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=false").json()
-print(pic)
 
