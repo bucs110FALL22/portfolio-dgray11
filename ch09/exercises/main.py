ea1695d9f1cb5907b41a68da68fdf0767ae0eff8
dgray11 <112587140+dgray11@users.noreply.github.com>
2022-12-01 00:00:56 +0000

    Exercise: Using Web APIs

--- ch09/exercises/main.py
+++ ch09/exercises/main.py
@@ -1,1 +1,8 @@
+import requests
+
+#facts = requests.get("https://dog-api.kinduff.com/api/facts").json()
+#print(facts.get("facts"))
+
+pic = requests.get("http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=false").json()
+print(pic)
 
